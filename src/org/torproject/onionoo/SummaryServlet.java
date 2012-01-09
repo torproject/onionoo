8694450d64ad8617e6849b63ac4d410801227572
Karsten Loesing <karsten.loesing@gmx.net>
2012-01-09 15:21:04 +0100

    Implement two simple servlets to serve requests.

---
+++ src/org/torproject/onionoo/SummaryServlet.java
@@ -0,0 +1,256 @@
+/* Copyright 2012 The Tor Project
+ * See LICENSE for licensing information */
+package org.torproject.onionoo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SummaryServlet extends HttpServlet {
+
+  public void init() {
+    this.readSummaryFile();
+  }
+
+  long summaryFileLastModified = 0L;
+  boolean readSummaryFile = false;
+  private String versionLine = null, validAfterLine = null,
+      freshUntilLine = null;
+  private List<String> relayLines = new ArrayList<String>(),
+      bridgeLines = new ArrayList<String>();
+  private void readSummaryFile() {
+    File summaryFile = new File("/srv/onionoo/out/summary.json");
+    if (!summaryFile.exists()) {
+      readSummaryFile = false;
+      return;
+    }
+    if (summaryFile.lastModified() > this.summaryFileLastModified) {
+      try {
+        BufferedReader br = new BufferedReader(new FileReader(
+            summaryFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+          if (line.startsWith("{\"version\":")) {
+            this.versionLine = line;
+          } else if (line.startsWith("\"valid_after\":")) {
+            this.validAfterLine = line;
+          } else if (line.startsWith("\"fresh_until\":")) {
+            this.freshUntilLine = line;
+          } else if (line.startsWith("\"relays\":")) {
+            while ((line = br.readLine()) != null && !line.equals("],")) {
+              this.relayLines.add(line);
+            }
+          } else if (line.startsWith("\"bridges\":")) {
+            while ((line = br.readLine()) != null && !line.equals("]}")) {
+              this.bridgeLines.add(line);
+            }
+          }
+        }
+        br.close();
+      } catch (IOException e) {
+        return;
+      }
+    }
+    this.summaryFileLastModified = summaryFile.lastModified();
+    this.readSummaryFile = true;
+  }
+
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response) throws IOException, ServletException {
+
+    this.readSummaryFile();
+    if (!this.readSummaryFile) {
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      return;
+    }
+
+    PrintWriter out = response.getWriter();
+    String uri = request.getRequestURI();
+    if (uri.equals("/onionoo/summary/all")) {
+      this.writeHeader(out);
+      this.writeAllRelays(out);
+      this.writeAllBridges(out);
+    } else if (uri.equals("/onionoo/summary/running")) {
+      this.writeHeader(out);
+      this.writeRunningRelays(out);
+      this.writeRunningBridges(out);
+    } else if (uri.equals("/onionoo/summary/relays")) {
+      this.writeHeader(out);
+      this.writeAllRelays(out);
+      this.writeNoBridges(out);
+    } else if (uri.equals("/onionoo/summary/bridges")) {
+      this.writeHeader(out);
+      this.writeNoRelays(out);
+      this.writeAllBridges(out);
+    } else if (uri.startsWith("/onionoo/summary/search/")) {
+      String searchParameter = this.parseSearchParameter(uri.substring(
+          "/onionoo/summary/search/".length()));
+      if (searchParameter == null) {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+      this.writeHeader(out);
+      this.writeMatchingRelays(out, searchParameter);
+      this.writeNoBridges(out);
+    } else if (uri.startsWith("/onionoo/summary/lookup/")) {
+      Set<String> fingerprintParameters = this.parseFingerprintParameters(
+          uri.substring("/onionoo/summary/lookup/".length()));
+      if (fingerprintParameters == null) {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+      this.writeHeader(out);
+      this.writeRelaysWithFingerprints(out, fingerprintParameters);
+      this.writeBridgesWithFingerprints(out, fingerprintParameters);
+    } else {
+      out.println("not supported");
+    }
+    out.close();
+  }
+
+  private static Pattern searchParameterPattern =
+      Pattern.compile("^[0-9a-zA-Z\\.]{1,40}$");
+  private String parseSearchParameter(String parameter) {
+    if (!searchParameterPattern.matcher(parameter).matches()) {
+      return null;
+    }
+    return parameter;
+  }
+
+  private static Pattern fingerprintParameterPattern =
+      Pattern.compile("^[a-zA-Z]+$");
+  private Set<String> parseFingerprintParameters(String parameter) {
+    if (!searchParameterPattern.matcher(parameter).matches()) {
+      return null;
+    }
+    Set<String> parsedFingerprints = new HashSet<String>();
+    if (parameter.length() != 40) {
+      return null;
+    }
+    parsedFingerprints.add(parameter);
+    return parsedFingerprints;
+  }
+
+  private void writeHeader(PrintWriter out) {
+    out.println(this.versionLine);
+    out.println(this.validAfterLine);
+    out.println(this.freshUntilLine);
+  }
+
+  private void writeAllRelays(PrintWriter out) {
+    out.println("\"relays\":[");
+    for (String line : this.relayLines) {
+      out.println(line);
+    }
+    out.println("],");
+  }
+
+  private void writeRunningRelays(PrintWriter out) {
+    out.print("\"relays\":[");
+    int written = 0;
+    for (String line : this.relayLines) {
+      if (line.contains("\"r\":true")) {
+        out.print((written++ > 0 ? ",\n" : "\n")
+            + (line.endsWith(",") ? line.substring(0, line.length() - 1)
+            : line));
+      }
+    }
+    out.println("\n],");
+  }
+
+  private void writeNoRelays(PrintWriter out) {
+    out.println("\"relays\":[");
+    out.println("],");
+  }
+
+  private void writeMatchingRelays(PrintWriter out, String searchTerm) {
+    out.print("\"relays\":[");
+    int written = 0;
+    for (String line : this.relayLines) {
+      if (line.toLowerCase().contains("\"n\":\""
+          + searchTerm.toLowerCase()) ||
+          ("unnamed".startsWith(searchTerm.toLowerCase()) &&
+          line.startsWith("{\"f\":")) ||
+          line.contains("\"f\":\"" + searchTerm.toUpperCase()) ||
+          line.substring(line.indexOf("\"a\":[")).contains("\""
+          + searchTerm)) {
+        out.print((written++ > 0 ? ",\n" : "\n")
+            + (line.endsWith(",") ? line.substring(0, line.length() - 1)
+            : line));
+      }
+    }
+    out.println("\n],");
+  }
+
+  private void writeRelaysWithFingerprints(PrintWriter out,
+      Set<String> fingerprints) {
+    out.print("\"relays\":[");
+    int written = 0;
+    for (String line : this.relayLines) {
+      for (String fingerprint : fingerprints) {
+        if (line.contains("\"f\":\"" + fingerprint.toUpperCase()
+            + "\",")) {
+          out.print((written++ > 0 ? ",\n" : "\n")
+              + (line.endsWith(",") ? line.substring(0, line.length() - 1)
+              : line));
+          break;
+        }
+      }
+    }
+    out.println("\n],");
+  }
+
+  private void writeAllBridges(PrintWriter out) {
+    out.println("\"bridges\":[");
+    for (String line : this.bridgeLines) {
+      out.println(line);
+    }
+    out.println("]}");
+  }
+
+  private void writeRunningBridges(PrintWriter out) {
+    out.print("\"bridges\":[");
+    int written = 0;
+    for (String line : this.bridgeLines) {
+      if (line.contains("\"r\":true")) {
+        out.print((written++ > 0 ? ",\n" : "\n")
+            + (line.endsWith(",") ? line.substring(0, line.length() - 1)
+            : line));
+      }
+    }
+    out.println("\n]}");
+  }
+
+  private void writeNoBridges(PrintWriter out) {
+    out.println("\"bridges\":[");
+    out.println("]}");
+  }
+
+  private void writeBridgesWithFingerprints(PrintWriter out,
+      Set<String> fingerprints) {
+    out.println("\"bridges\":[");
+    for (String line : this.bridgeLines) {
+      for (String fingerprint : fingerprints) {
+        if (line.contains("\"h\":\"" + fingerprint.toUpperCase()
+            + "\",")) {
+          out.println(line);
+          break;
+        }
+      }
+    }
+    out.println("]}");
+  }
+}
+
