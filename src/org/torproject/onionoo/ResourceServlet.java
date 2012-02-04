ba6bda102a698d4bb11b08d34853ab8210941a62
Karsten Loesing <karsten.loesing@gmx.net>
2012-02-04 10:10:05 +0100

    Add a generated serialVersionUID to make Eclipse happy.

--- src/org/torproject/onionoo/ResourceServlet.java
+++ src/org/torproject/onionoo/ResourceServlet.java
@@ -20,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ResourceServlet extends HttpServlet {
 
+  private static final long serialVersionUID = 7236658979947465319L;
+
   public void init() {
     this.readSummaryFile();
   }
