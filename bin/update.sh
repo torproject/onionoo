c40402127d11c8947a5dbcae933bc5f0b1975aac
Karsten Loesing <karsten.loesing@gmx.net>
2014-05-24 22:47:48 +0200

    Rewrite rsync command to exclude concatenated descriptors.
    See 73bb381 in metrics-db.git.

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -arz --delete --exclude 'relay-descriptors/votes' --exclude 'relay-descriptors/microdescs' --exclude 'torperf' metrics.torproject.org::metrics-recent in
+rsync -az --delete metrics.torproject.org::'metrics-recent/relay-descriptors/consensuses/ metrics-recent/relay-descriptors/server-descriptors/ metrics-recent/relay-descriptors/extra-infos/ metrics-recent/bridge-descriptors/statuses/ metrics-recent/bridge-descriptors/server-descriptors/ metrics-recent/bridge-descriptors/extra-infos/ metrics-recent/bridge-pool-assignments/ metrics-recent/exit-lists' in
 ant run >> log
 
