ed10c7c37262c647c3d78a5f0fc55a7bf4296ab2
Karsten Loesing <karsten.loesing@gmx.net>
2014-08-15 18:39:09 +0200

    Move UptimeStatusTest to sub package.

--- src/test/java/org/torproject/onionoo/docs/UptimeStatusTest.java
+++ src/test/java/org/torproject/onionoo/docs/UptimeStatusTest.java
@@ -1,6 +1,6 @@
 /* Copyright 2014 The Tor Project
  * See LICENSE for licensing information */
-package org.torproject.onionoo;
+package org.torproject.onionoo.docs;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
