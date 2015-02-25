torproject/onionoo

commit f660e24be80b2efed1423b9ebf912377ccea49d5

Avoid String.replaceAll() because of memory issues.
From a quick look at the sources it seems that they're using
String.indexOf() to find patterns rather than regular expressions which
is what String.replaceAll() uses.

src/main/java/org/torproject/onionoo/docs/DetailsDocument.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class DetailsDocument extends Document {
 
@@ -18,12 +19,12 @@ public class DetailsDocument extends Document {
    * we'll have to do is to change back the '\\' that Gson writes for the
    * '\'. */
   private static String escapeJSON(String s) {
-    return s == null ? null :
-        StringEscapeUtils.escapeJavaScript(s).replaceAll("\\\\'", "'");
+    return StringUtils.replaceEach(StringEscapeUtils.escapeJavaScript(s),
+        new String[] { "\\\\'" }, new String[] { "'" });
   }
   private static String unescapeJSON(String s) {
-    return s == null ? null :
-        StringEscapeUtils.unescapeJavaScript(s.replaceAll("'", "\\'"));
+    return StringEscapeUtils.unescapeJavaScript(StringUtils.replaceEach(s,
+        new String[] { "'" }, new String[] { "\\'" }));
   }
 
   private String nickname;

src/main/java/org/torproject/onionoo/docs/DetailsStatus.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class DetailsStatus extends Document {
 
@@ -18,12 +19,12 @@ public class DetailsStatus extends Document {
    * we'll have to do is to change back the '\\' that Gson writes for the
    * '\'. */
   private static String escapeJSON(String s) {
-    return s == null ? null :
-        StringEscapeUtils.escapeJavaScript(s).replaceAll("\\\\'", "'");
+    return StringUtils.replaceEach(StringEscapeUtils.escapeJavaScript(s),
+        new String[] { "\\\\'" }, new String[] { "'" });
   }
   private static String unescapeJSON(String s) {
-    return s == null ? null :
-        StringEscapeUtils.unescapeJavaScript(s.replaceAll("'", "\\'"));
+    return StringEscapeUtils.unescapeJavaScript(StringUtils.replaceEach(s,
+        new String[] { "'" }, new String[] { "\\'" }));
   }
 
   private String desc_published;

src/main/java/org/torproject/onionoo/docs/DocumentStore.java
@@ -21,6 +21,7 @@ import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.torproject.onionoo.util.FormattingUtils;
@@ -275,8 +276,8 @@ public class DocumentStore {
        * objects are escaped JSON, e.g., \u00F2.  When Gson serlializes
        * this string, it escapes the \ to \\, hence writes \\u00F2.  We
        * need to undo this and change \\u00F2 back to \u00F2. */
-      documentString = gson.toJson(document).replaceAll("\\\\\\\\u",
-          "\\\\u");
+      documentString = StringUtils.replace(gson.toJson(document),
+          "\\\\\\\\u", "\\\\u");
       /* Existing details statuses don't contain opening and closing curly
        * brackets, so we should remove them from new details statuses,
        * too. */

commit 24d9e5128c8d823a86d1ccc011f6d058e414c709

Use Apache Common Lang's StringUtils.replace correctly.
When we switched from String.replaceAll to StringUtils.replaceEach for
performance reasons, we forgot to change the passed regular expression
into a simple string.  That's why some of our replacements didn't work out
as expected.

Bug introduced in f660e24be80b2efed1423b9ebf912377ccea49d5.

src/main/java/org/torproject/onionoo/docs/DetailsDocument.java
@@ -22,7 +22,7 @@ public class DetailsDocument extends Document {
    * '\'. */
   private static String escapeJSON(String s) {
     return StringUtils.replaceEach(StringEscapeUtils.escapeEcmaScript(s),
-        new String[] { "\\\\'" }, new String[] { "'" });
+        new String[] { "\\'" }, new String[] { "'" });
   }
   private static String unescapeJSON(String s) {
     return StringEscapeUtils.unescapeEcmaScript(StringUtils.replaceEach(s,

src/main/java/org/torproject/onionoo/docs/DetailsStatus.java
@@ -22,7 +22,7 @@ public class DetailsStatus extends Document {
    * '\'. */
   private static String escapeJSON(String s) {
     return StringUtils.replaceEach(StringEscapeUtils.escapeEcmaScript(s),
-        new String[] { "\\\\'" }, new String[] { "'" });
+        new String[] { "\\'" }, new String[] { "'" });
   }
   private static String unescapeJSON(String s) {
     return StringEscapeUtils.unescapeEcmaScript(StringUtils.replaceEach(s,

src/main/java/org/torproject/onionoo/docs/DocumentStore.java
@@ -316,7 +316,7 @@ public class DocumentStore {
        * this string, it escapes the \ to \\, hence writes \\u00F2.  We
        * need to undo this and change \\u00F2 back to \u00F2. */
       documentString = StringUtils.replace(gson.toJson(document),
-          "\\\\\\\\u", "\\\\u");
+          "\\\\u", "\\u");
       /* Existing details statuses don't contain opening and closing curly
        * brackets, so we should remove them from new details statuses,
        * too. */

src/main/java/org/torproject/onionoo/server/ResponseBuilder.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.torproject.onionoo.docs.BandwidthDocument;
 import org.torproject.onionoo.docs.ClientsDocument;
 import org.torproject.onionoo.docs.DetailsDocument;
@@ -274,7 +275,7 @@ public class ResponseBuilder {
         /* Whenever we provide Gson with a string containing an escaped
          * non-ASCII character like \u00F2, it escapes the \ to \\, which
          * we need to undo before including the string in a response. */
-        return gson.toJson(dd).replaceAll("\\\\\\\\u", "\\\\u");
+        return StringUtils.replace(gson.toJson(dd), "\\\\u", "\\u");
       } else {
         // TODO We should probably log that we didn't find a details
         // document that we expected to exist.