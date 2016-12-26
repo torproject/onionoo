0442032f24c8764d6c055ed3b9f4e047fbaa886e
iwakeh <iwakeh@torproject.org>
2016-12-26 13:27:48 +0100

    Implements task-20596: use metrics-base and reduced build.xml, added bootstrap script.

---
+++ src/main/resources/bootstrap-development.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Copyright 2016 The Tor Project
+# See LICENSE for licensing information
+#
+# Use for submodule initialization and checkout.
+# Only necessary after very first checkout without recursion.
+#
+git submodule update --init --remote
+
