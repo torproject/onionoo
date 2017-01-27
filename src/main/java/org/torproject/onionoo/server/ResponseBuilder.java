e9cc30cdbff2744120ea3a7ff10ee18ceb25399c
Karsten Loesing <karsten.loesing@gmx.net>
2017-01-27 10:11:19 +0100

    Schedule next major version.

--- src/main/java/org/torproject/onionoo/server/ResponseBuilder.java
+++ src/main/java/org/torproject/onionoo/server/ResponseBuilder.java
@@ -104,7 +104,7 @@ public class ResponseBuilder {
 
   private static final String PROTOCOL_VERSION = "3.2";
 
-  private static final String NEXT_MAJOR_VERSION_SCHEDULED = null;
+  private static final String NEXT_MAJOR_VERSION_SCHEDULED = "2017-02-27";
 
   private void writeRelays(List<SummaryDocument> relays, PrintWriter pw) {
     this.write(pw, "{\"version\":\"%s\",\n", PROTOCOL_VERSION);
